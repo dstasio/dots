607010ba2c593b83e453d89786269c7bf947a7c9
Davide <doctegl@gmail.com>
2025-05-22 07:20:46 +0200

    Vim & more incomplete stuff

--- make_links.sh
+++ make_links.sh
@@ -2,9 +2,11 @@
 
 script_dir=$(realpath $(dirname "$0"))
 
-ln -s $script_dir/hypr   $HOME/.config/hypr
-ln -s $script_dir/kitty  $HOME/.config/kitty
-ln -s $script_dir/waybar $HOME/.config/waybar
+# ln -s $script_dir/hypr   $HOME/.config/hypr
+# ln -s $script_dir/kitty  $HOME/.config/kitty
+# ln -s $script_dir/waybar $HOME/.config/waybar
 
+sudo stow -v -t /etc -d "$script_dir/etc" -R "greetd"
 
-
+sudo stow -v -t ~ -d "$script_dir" -R "git"
+sudo stow -v -t ~ -d "$script_dir" -R "vim"
