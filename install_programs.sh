30ae18956b65edea1e73d8c5065bd1406bc528a0
Davide <doctegl@gmail.com>
2025-12-27 12:42:28 +0100

    Nobara 0

--- install_programs.sh
+++ install_programs.sh
@@ -1,4 +1,14 @@
 # wl-clipboard: wl-copy, wl-paste
 
+program_list=
 
-sudo dnf install wl-clipboard
+append_program () { program_list+="$1 "; }
+
+append_program fish
+append_program wl-clipboard
+
+sudo dnf install dnf-plugins-core
+sudo dnf copr enable atim/lazygit
+append_program lazygit
+
+sudo dnf install $program_list
--- make_links.sh
+++ make_links.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# if [ "$#" -eq 0 ]; then
+    # No arguments given.
+# fi
+
 script_dir=$(realpath $(dirname "$0"))
 
 # ln -s $script_dir/hypr   $HOME/.config/hypr
 # ln -s $script_dir/kitty  $HOME/.config/kitty
 # ln -s $script_dir/waybar $HOME/.config/waybar
 
-sudo stow -v -t /etc -d "$script_dir/etc" -R "greetd"
+# sudo stow -v -t /etc -d "$script_dir/etc" -R "greetd"
 
 sudo stow -v -t ~ -d "$script_dir" -R "git"
 sudo stow -v -t ~ -d "$script_dir" -R "vim"
+sudo stow -v -t ~ -d "$script_dir" -R "bash"
